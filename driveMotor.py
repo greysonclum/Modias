449a3abf324a97f981bbab0bd4caa47337c02a5b
Greyson Clum <clum.grey@gmail.com>
2019-09-25 12:00:17 -0700

    Added 0-360 value

--- driveMotor.py
+++ driveMotor.py
@@ -39,20 +39,20 @@ wheelBL_val = 0;
 wheelBR_val = 0;
 
 GPIO.setup(armed, GPIO.OUT)
-GPIO.setup(wheelFL, GPIO.OUT)
-GPIO.setup(wheelFR, GPIO.OUT)
-GPIO.setup(wheelBL, GPIO.OUT)
-GPIO.setup(wheelBR, GPIO.OUT)
+GPIO.setup(motorFL, GPIO.OUT)
+GPIO.setup(motorFR, GPIO.OUT)
+GPIO.setup(motorBL, GPIO.OUT)
+GPIO.setup(motorBR, GPIO.OUT)
 
 #Arm switch must be on to run motor. Default off if no connection
 def arm_bot():
     GPIO.output(armed, 1)
 
 def kill_motors():
-    GPIO.output(wheelFL, 0)
-    GPIO.output(wheelFR, 0)
-    GPIO.output(wheelBL, 0)
-    GPIO.output(wheelBR, 0)
+    GPIO.output(motorFL, 0)
+    GPIO.output(motorFR, 0)
+    GPIO.output(motorBL, 0)
+    GPIO.output(motorBR, 0)
     #print('Motors off');
 
 def map(val, in_min, in_max, out_min, out_max):
@@ -64,13 +64,13 @@ def input_direction():
     print(get_angle)
     sleep(2);
 
-def setmotors():
-    GPIO.output(wheelFL, wheelFL_val)
-    GPIO.output(wheelFR, wheelFR_val)
-    GPIO.output(wheelBL, wheelBL_val)
-    GPIO.output(wheelBR, wheelBR_val)
+def setmotors(motorFL_val,motorFR_val,motor_BL_val,motorBR_val):
+    GPIO.output(motorFL, motorFL_val)
+    GPIO.output(motorFR, motorFR_val)
+    GPIO.output(motorBL, motorBL_val)
+    GPIO.output(motorBR, motorBR_val)
 
-#Button and axis variables
+#Button and axis variables (Top = -1, Bottom = 1; Left = -1, Right = 1)
 axis_names_list = ['Lstick_leftright','Lstick_updown','L2','Rstick_leftright','Rstick_updown','R2'];
 axis_values = [];
 but_names_list = ['X','O','TRI','SQ','L1','R1','L2','R2','SEL','SRT','PS','LCK','RCK','UP','DN','L','R'];
@@ -91,14 +91,22 @@ while armed == 1:
                 if axis_names_list[k] == 'Lstick_updown' and axis_val != 0:
                     #print(str(axis_names_list[k])+ ' axis moved');
                     print(axis_val);
+                else:
+                    Lstick_updown = .00001; #prevent dividing by zero for arctan
                 if axis_names_list[k] == 'Lstick_leftright' and axis_val != 0: #and 0.1 <= axis_val <= -0.1:
                     #print(str(axis_names_list[k])+ ' axis moved');
                     print(axis_val);
+                else:
+                    Lstick_leftright = .00001;
                 if axis_names_list[k] == 'Rstick_updown' and axis_val != 0:
                     Rstick_updown = axis_val;
                     #print(str(axis_names_list[k])+ ' axis moved');
+                else:
+                    Rstick_updown = .00001;
                 if axis_names_list[k] == 'Rstick_leftright' and axis_val != 0:
                     Rstick_leftright = axis_val;
+                else:
+                    Rstick_updown = .00001;
                     #print(str(axis_names_list[k])+ ' axis moved');
                 if axis_names_list[k] == 'L2' and axis_val != -1 and axis_val != 0:
                     #print(str(axis_names_list[k])+ ' axis moved');
@@ -156,15 +164,33 @@ while armed == 1:
                 else:
                     kill_motors();
 
-        #Determine what direction Modias moves
-        degree = np.arctan((Rstick_updown/Rstick_leftright)*(180/np.pi));
-        motor1 = 0;
-        motor2 = 0;
-        motor3 = 0;
-        motor4 = 0;
+        #Determine what direction Modias moves. Should set theta to 0-360 value.
+        if Rstick_updown <= 0 and Rstick_leftright <= 0;
+            theta = np.arctan((Rstick_updown/Rstick_leftright));
+        if Rstick_updown <= 0 and Rstick_leftright >= 0;
+            theta = np.arctan((Rstick_updown/Rstick_leftright))+180;
+        if Rstick_updown >= 0 and Rstick_leftright >= 0;
+            theta = abs(np.arctan((Rstick_updown/Rstick_leftright)))+180;
+        if Rstick_updown >= 0 and Rstick_leftright <= 0;
+            theta = np.arctan((Rstick_updown/Rstick_leftright))+360;
+
+        #Determine motor values
+        theta = np.arctan((Rstick_updown/Rstick_leftright)); #in radians
+        mag = np.sqrt(Rstick_updown**2+Rstick_leftright**2);
+        motorFL_val = 255*mag*cos(theta+(45/180*np.pi));
+        motorFR_val = 0;
+        motorBL_val = 0;
+        motorBR_val = 0;
+
+        #LED position
+        #map theta (0-360) to number of LEDs (29?).
+        #power LED
+
     #Correct motors for direction
     #motor_correct()
     #Set motor to last value above
-    setmotors()
+    setmotors(motorFL_val, motorFR_val, motorBL_val, motorBR_val)
+
     # Motor values can only be updated every 20ms
     sleep(.2);
+
